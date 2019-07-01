ea9860a053572677e64cc2fc790ab7a0470175be
rdhaundiyal <rdhaundiyal@hotmail.com>
2019-07-01 16:59:50 +0530

    added handler

---
+++ Src/Services/News/Demo.Microservices.Services.News/Handlers/NewsQueryHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Microservices.Services.News.Handlers
+{
+    public class NewsQueryHandler
+    {
+    }
+}
