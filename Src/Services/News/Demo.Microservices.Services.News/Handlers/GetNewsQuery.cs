91c6aace62eb1e28720648f4eb25a690faa395bb
rdhaundiyal <rdhaundiyal@hotmail.com>
2019-07-09 17:52:29 +0530

    commit

---
+++ Src/Services/News/Demo.Microservices.Services.News/Handlers/GetNewsQuery.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Demo.Microservices.Services.News.Entities;
+
+namespace Demo.Microservices.Services.News.Handlers
+{
+    public class GetNewsQuery : Query<Entities.News>
+    {
+        public GetNewsQuery(Entities.News entity) : base(entity)
+        {
+        }
+
+        public override Entities.News Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Src/Services/News/Demo.Microservices.Services.News/Handlers/PublishNewsCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+using Demo.Microservices.Services.News.Entities;
+
+namespace Demo.Microservices.Services.News.Handlers
+{
+    public class PublishNewsCommand : Command<News.Entities.News>
+    {
+        public PublishNewsCommand(Entities.News entity) : base(entity)
+        {
+        }
+
+        public override Result Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
